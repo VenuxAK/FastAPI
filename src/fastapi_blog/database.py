b95042d015454a64b4d7b4b9a2a52f37de83d544
VenuxAK <shelby.ak68@gmail.com>
2026-08-25 14:53:32 +0630

    Convert to asynchronous app

--- src/fastapi_blog/database.py
+++ src/fastapi_blog/database.py
@@ -1,19 +1,26 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./posts.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./posts.db"
 
-engine = create_engine(
+engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- src/fastapi_blog/main.py
+++ src/fastapi_blog/main.py
@@ -1,10 +1,18 @@
+from contextlib import asynccontextmanager
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
+
+# from fastapi.exception_handlers import (
+#     http_exception_handler,
+#     request_validation_exception_handler,
+# )
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy import Select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
+# from starlette.exceptions import HTTPException as StarletteHTTPException
 import fastapi_blog.models
 from fastapi_blog.database import Base, engine, get_db
 from fastapi_blog.schemas import (
@@ -16,12 +24,26 @@ from fastapi_blog.schemas import (
     UserUpdate,
 )
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    # Startup
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+    yield
+    # Shutdown
+    await engine.dispose()
+
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/media", StaticFiles(directory="media"), name="media")
+
+# @app.exception_handlers(StarletteHTTPException)
+# def general_http_exception_handler(request: Request)
 
 
 @app.get("/")
@@ -36,10 +58,10 @@ def main() -> dict:
 @app.post(
     "/api/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED
 )
-def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)]):
 
     # Validate username
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(
             fastapi_blog.models.User.username == user.username
         )
@@ -49,7 +71,7 @@ def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
         raise HTTPException(status_code=422, detail="Username already taken.")
 
     # Validate email
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(
             fastapi_blog.models.User.email == user.email
         )
@@ -60,8 +82,8 @@ def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
 
     new_user = fastapi_blog.models.User(username=user.username, email=user.email)
     db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    await db.commit()
+    await db.refresh(new_user)
 
     return new_user
 
@@ -70,8 +92,8 @@ def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
 @app.get(
     "/api/users/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK
 )
-def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
+async def get_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(fastapi_blog.models.User.id == user_id)
     )
     user = result.scalars().first()
@@ -88,9 +110,9 @@ def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
     response_model=list[PostResponse],
     status_code=status.HTTP_200_OK,
 )
-def get_user_posts(user_id: int, db: Annotated[Session, Depends(get_db)]):
+async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
 
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(fastapi_blog.models.User.id == user_id)
     )
     user = result.scalars().first()
@@ -98,10 +120,10 @@ def get_user_posts(user_id: int, db: Annotated[Session, Depends(get_db)]):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    result = db.execute(
-        Select(fastapi_blog.models.Post).where(
-            fastapi_blog.models.Post.user_id == user.id
-        )
+    result = await db.execute(
+        Select(fastapi_blog.models.Post)
+        .where(fastapi_blog.models.Post.user_id == user.id)
+        .options(selectinload(fastapi_blog.models.Post.author))
     )
 
     posts = result.scalars().all()
@@ -113,8 +135,10 @@ def get_user_posts(user_id: int, db: Annotated[Session, Depends(get_db)]):
 @app.get(
     "/api/users/{user_id}/posts/{post_id}", response_model=PostResponse, status_code=200
 )
-def get_user_post(user_id: int, post_id: int, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
+async def get_user_post(
+    user_id: int, post_id: int, db: Annotated[AsyncSession, Depends(get_db)]
+):
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(fastapi_blog.models.User.id == user_id)
     )
     user = result.scalars().first()
@@ -122,18 +146,20 @@ def get_user_post(user_id: int, post_id: int, db: Annotated[Session, Depends(get
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.Post).where(fastapi_blog.models.Post.id == post_id)
     )
     post = result.scalars().first()
     if not post:
         raise HTTPException(404, detail="Post not found")
 
-    result = db.execute(
-        Select(fastapi_blog.models.Post).where(
+    result = await db.execute(
+        Select(fastapi_blog.models.Post)
+        .where(
             fastapi_blog.models.Post.id == post_id
             and fastapi_blog.models.Post.user_id == user_id
         )
+        .options(selectinload(fastapi_blog.models.Post.author))
     )
 
     post = result.scalars().first()
@@ -147,10 +173,10 @@ def get_user_post(user_id: int, post_id: int, db: Annotated[Session, Depends(get
     response_model=UserResponse,
     status_code=status.HTTP_201_CREATED,
 )
-def update_user_profile(
-    user_id: int, data: UserUpdate, db: Annotated[Session, Depends(get_db)]
+async def update_user_profile(
+    user_id: int, data: UserUpdate, db: Annotated[AsyncSession, Depends(get_db)]
 ):
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(fastapi_blog.models.User.id == user_id)
     )
     user = result.scalars().first()
@@ -159,7 +185,7 @@ def update_user_profile(
         raise HTTPException(status_code=404, detail="User not found.")
 
     if data.email is not None and user.email != data.email:
-        result = db.execute(
+        result = await db.execute(
             Select(fastapi_blog.models.User).where(
                 fastapi_blog.models.User.email == data.email
             )
@@ -170,7 +196,7 @@ def update_user_profile(
             raise HTTPException(status_code=400)
 
     if data.username is not None and user.username != data.username:
-        result = db.execute(
+        result = await db.execute(
             Select(fastapi_blog.models.User).where(
                 fastapi_blog.models.User.username == data.username
             )
@@ -187,15 +213,17 @@ def update_user_profile(
     if data.image_file is not None:
         user.image_file = data.image_file
 
-    db.commit()
-    db.refresh(user)
+    await db.commit()
+    await db.refresh(user)
     return user
 
 
 # Delete user profile
 @app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user_profile(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
+async def delete_user_profile(
+    user_id: int, db: Annotated[AsyncSession, Depends(get_db)]
+):
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(fastapi_blog.models.User.id == user_id)
     )
     user = result.scalars().first()
@@ -206,8 +234,8 @@ def delete_user_profile(user_id: int, db: Annotated[Session, Depends(get_db)]):
     if user.posts:
         raise HTTPException(400, "User cannot be deleted")
 
-    db.delete(user)
-    db.commit()
+    await db.delete(user)
+    await db.commit()
 
 
 ################ Post #######################
@@ -217,8 +245,12 @@ def delete_user_profile(user_id: int, db: Annotated[Session, Depends(get_db)]):
 @app.get(
     "/api/posts", response_model=list[PostResponse], status_code=status.HTTP_200_OK
 )
-def get_all_posts(db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(Select(fastapi_blog.models.Post))
+async def get_all_posts(db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(
+        Select(fastapi_blog.models.Post).options(
+            selectinload(fastapi_blog.models.Post.author)
+        )
+    )
     posts = result.scalars().all()
 
     return posts
@@ -228,12 +260,12 @@ def get_all_posts(db: Annotated[Session, Depends(get_db)]):
 @app.get(
     "/api/posts/{post_id}", response_model=PostResponse, status_code=status.HTTP_200_OK
 )
-def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
+async def get_post(post_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
 
-    result = db.execute(
-        Select(fastapi_blog.models.Post).where(
-            fastapi_blog.models.Post.id == int(post_id)
-        )
+    result = await db.execute(
+        Select(fastapi_blog.models.Post)
+        .where(fastapi_blog.models.Post.id == int(post_id))
+        .options(selectinload(fastapi_blog.models.Post.author))
     )
 
     post = result.scalars().first()
@@ -248,9 +280,9 @@ def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
 @app.post(
     "/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED
 )
-def create_post(post: PostCreate, db: Annotated[Session, Depends(get_db)]):
+async def create_post(post: PostCreate, db: Annotated[AsyncSession, Depends(get_db)]):
 
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.User).where(
             fastapi_blog.models.User.id == post.user_id
         )
@@ -265,8 +297,8 @@ def create_post(post: PostCreate, db: Annotated[Session, Depends(get_db)]):
     )
 
     db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
+    await db.commit()
+    await db.refresh(new_post)
     return new_post
 
 
@@ -276,10 +308,10 @@ def create_post(post: PostCreate, db: Annotated[Session, Depends(get_db)]):
     response_model=PostResponse,
     status_code=status.HTTP_201_CREATED,
 )
-def update_post(
-    post_id: int, data: PostUpdate, db: Annotated[Session, Depends(get_db)]
+async def update_post(
+    post_id: int, data: PostUpdate, db: Annotated[AsyncSession, Depends(get_db)]
 ):
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.Post).where(fastapi_blog.models.Post.id == post_id)
     )
     post = result.scalars().first()
@@ -293,8 +325,8 @@ def update_post(
     post.title = data.title
     post.content = data.content
 
-    db.commit()
-    db.refresh(post)
+    await db.commit()
+    await db.refresh(post)
     return post
 
 
@@ -304,10 +336,10 @@ def update_post(
     response_model=PostResponse,
     status_code=status.HTTP_201_CREATED,
 )
-def update_partial_data_post(
-    post_id: int, data: PostUpdate, db: Annotated[Session, Depends(get_db)]
+async def update_partial_data_post(
+    post_id: int, data: PostUpdate, db: Annotated[AsyncSession, Depends(get_db)]
 ):
-    result = db.execute(
+    result = await db.execute(
         Select(fastapi_blog.models.Post).where(fastapi_blog.models.Post.id == post_id)
     )
     post = result.scalars().first()
@@ -322,15 +354,15 @@ def update_partial_data_post(
     for key, value in updated_data.items():
         setattr(post, key, value)
 
-    db.commit()
-    db.refresh(post)
+    await db.commit()
+    await db.refresh(post)
     return post
 
 
 # Delete post
 @app.delete("/api/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(
+async def delete_post(post_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(
         Select(fastapi_blog.models.Post).where(fastapi_blog.models.Post.id == post_id)
     )
     post = result.scalars().first()
@@ -338,5 +370,5 @@ def delete_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
     if not post:
         raise HTTPException(404, "Post not found")
 
-    db.delete(post)
-    db.commit()
+    await db.delete(post)
+    await db.commit()
